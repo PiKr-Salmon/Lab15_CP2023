2e85b6bb3c33aeb586ae4527e96910aaed1c050c
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-02-01 21:54:59 +0700

    lab15_1 complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,36 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int *z = *&x;
 
+	cout << a  << " "<< b  << " " << c << " " << &x  << " " << &y << " " << &z << endl;
 	
+	int ab = *&b;
+	int ac = *&c;
+	cout << &a  << " "<< &ab  << " " << &ac << " " << &x  << " " << &y << " " << &z << endl;
 	
+	c = 'F';
+	b = c ;
+	cout << a  << " "<< b << " " << c << " " << &x  << " " << &y << " " << &z << endl;
+
+	*y = 'W' ;
+	b = *y ;
+	cout << a  << " "<< b << " " << c << " " << &x  << " " << &y << " " << &z << endl;
+
+	*x = 6;
+	a = *x;
+	cout << a  << " "<< b << " " << c << " " << &x  << " " << &y << " " << &z << endl;
+
+	*z = 7;
+	*x = *z;
+	a = *x;
+	cout << a  << " "<< b << " " << c << " " << &x  << " " << &y << " " << &z << endl;
+
 	return 0;
 }
